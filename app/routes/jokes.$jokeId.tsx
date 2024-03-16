1bc1abe4ab4391b463c966f5719022d2231313cb
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-03-16 23:12:35 +0300

    feat: geting koke by id

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -1,11 +1,27 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = async ({
+    params,
+}: LoaderFunctionArgs) => {
+    console.log(params); // <-- {jokeId: "123"}
+    const joke = await db.joke.findUnique({
+        where: { id: params.jokeId },
+    });
+    if (!joke) throw new Error("Joke notfound");
+    return json({ joke });
+};
+
 export default function JokeRoute() {
+    const data = useLoaderData<typeof loader>();
+
     return (
         <div>
             <p>Here's your hilarious joke:</p>
-            <p>
-                Why don't you find hippopotamuses hiding in trees?
-                They're really good at it.
-            </p>
+            <p>{data.joke.content}</p>
+            <Link to=".">"{data.joke.name}" Permalink</Link>
         </div>
+
     );
 }
