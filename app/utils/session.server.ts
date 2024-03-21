993c8cc9e6df05aa19296422d6ae85629375334d
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-03-21 10:04:33 +0300

    feat: setting up session

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,14 +1,44 @@
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
-import { db } from "./db.server"
+import { db } from "./db.server";
+import { createCookieSessionStorage, redirect } from "@remix-run/node";
 
 type LoginForm = {
   password: string;
   username: string;
+};
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "RJ_SESSION",
+    // normally you want this to be `secure: true`
+    // but that doesn't work on localhost for Safari
+    // https://web.dev/when-to-use-local-https/
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set("userId", userId);
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await storage.commitSession(session),
+    },
+  });
 }
 
 export async function login({ username, password }: LoginForm) {
-  const user = await db.user.findUnique({ where: { username } })
+  const user = await db.user.findUnique({ where: { username } });
   if (!user) {
     return null;
   }
@@ -17,5 +47,5 @@ export async function login({ username, password }: LoginForm) {
   if (!isCorrectPasword) {
     return null;
   }
-  return { id: user.id, username }
+  return { id: user.id, username };
 }
